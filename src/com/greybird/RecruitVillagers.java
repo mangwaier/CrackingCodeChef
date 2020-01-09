c7b63dbb22d8599a95c3223022463a6fde058bb3
mangwaier <18380592012@163.com>
2020-01-10 07:12:58 +0800

    coding more with codechef

---
+++ src/com/greybird/RecruitVillagers.java
@@ -0,0 +1,14 @@
+package com.greybird;
+
+import java.util.Scanner;
+
+public class RecruitVillagers {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        short m = scanner.nextShort();
+        short c = scanner.nextShort();
+        System.out.println(n + ", " + m + ", " + c);
+    }
+}
